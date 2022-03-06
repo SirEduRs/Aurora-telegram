c5ba375ffecb4088dc1c85e9ad86fae0df062cb1
Eduardo Rs <rodrigueseduardo1966@gmail.com>
2022-03-06 17:32:09 -0300

    Deixando mais arrumadinho...

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-import asyncio
-from datetime import datetime
-
 from os import environ
-from dotenv import load_dotenv
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
 
-load_dotenv('bot.env')
+load_dotenv("bot.env")
 
-bot: Bot = Bot(token=environ.get('BOT_TOKEN'))
+bot: Bot = Bot(token=environ.get("BOT_TOKEN"))
 dp: Dispatcher = Dispatcher(bot, storage=MemoryStorage())
 
 from Modules import Utils
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from bot import dp
 from aiogram import executor
 
-if __name__ == '__main__':
+from bot import dp
+
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
